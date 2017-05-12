1160e66308ce3ecac3be99a1362732b161bf65f1
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-05-12 11:42:02 +0100

    set first jquery command

--- js/interface.js
+++ js/interface.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+
   var thermostat = new Thermostat();
+
   $('#temperature').text(thermostat.temperature);
+
+  $('#psm_on').click(function( clickEvent ) {
+    
+    alert("click");
+
+  });
+
 });
