a5b7b30596db6cf80d7327f05adf82a525cf0269
Robert Jones <robert.g.jones@gmail.com>
2017-05-12 12:12:14 +0100

    Basic jQuery functionality on html buttons

--- js/interface.js
+++ js/interface.js
@@ -5,9 +5,30 @@ $(document).ready(function() {
   $('#temperature').text(thermostat.temperature);
 
   $('#psm_on').click(function( clickEvent ) {
-    
-    alert("click");
-
+    thermostat.powerSavingOn();
   });
+
+  $('#psm_off').click(function() {
+    thermostat.powerSavingOff();
+  });
+
+  $('#temp-up').on('click', function( clickEvent ) {
+    console.log(thermostat.currentTemperature());
+    thermostat.up();
+    console.log(thermostat.currentTemperature());
+  });
+
+  $('#temp-down').on('click', function() {
+    console.log(thermostat.currentTemperature());
+    thermostat.down();
+    console.log(thermostat.currentTemperature());
+  });
+
+  $('#reset').on('click', function() {
+    thermostat.resetTemp();
+    console.log(thermostat.currentTemperature());
+  });
+
+
 
 });
